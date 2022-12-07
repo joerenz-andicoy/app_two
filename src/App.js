7df3fa83cf4ffee16eb49d0e756dab96ca29cf01
JOSE_LORENZO_ANDICOy <andicoylorenzo@gmail.com>
2022-12-07 14:38:33 +0800

    first_git_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index";
+import { questions } from "./questions";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const handleButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  const handleRestart = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+          <button className="restart-button" onClick={handleRestart}>
+            Restart Quiz
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOptions) => (
+              <button
+                onClick={() => handleButtonClick(answerOptions.isCorrect)}
+              >
+                {answerOptions.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/questions.js
@@ -0,0 +1,39 @@
+export const questions = [
+  {
+    questionText: "What is the rarest M&M color?",
+    answerOptions: [
+      { answerText: "Red", isCorrect: false },
+      { answerText: "Blue", isCorrect: false },
+      { answerText: "Brown", isCorrect: true },
+      { answerText: "Green", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "In a website browser address bar, what does “www” stand for?",
+    answerOptions: [
+      { answerText: "Worl Wide Website", isCorrect: false },
+      { answerText: "World Wide Web", isCorrect: true },
+      { answerText: "World Whole Web", isCorrect: false },
+      { answerText: "World Whole Website", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "In what year were the first Air Jordan sneakers released?",
+    answerOptions: [
+      { answerText: "1984", isCorrect: true },
+      { answerText: "1988", isCorrect: false },
+      { answerText: "1979", isCorrect: false },
+      { answerText: "1975", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which African country was formerly known as Abyssinia?",
+    answerOptions: [
+      { answerText: "Egypt", isCorrect: false },
+      { answerText: "Libya", isCorrect: false },
+      { answerText: "Nigeria", isCorrect: false },
+      { answerText: "Ethiopia", isCorrect: true },
+    ],
+  },
+];
